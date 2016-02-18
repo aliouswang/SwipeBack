659c55a5c8402ec577673aeb8aa250a116a9842d
aliouswang <wsw622@126.com>
2016-02-18 16:31:38 +0800

    Initial commit

---
+++ app/src/main/java/com/aliouswang/swipeback/MainActivity.java
@@ -0,0 +1,35 @@
+package com.aliouswang.swipeback;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_view_drag_helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initView();
+    }
+
+    private void initView() {
+        btn_view_drag_helper = (Button) findViewById(R.id.btn_view_drag_helper);
+        btn_view_drag_helper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewDragerHelperDemoAct.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/aliouswang/swipeback/ViewDragerHelperDemoAct.java
@@ -0,0 +1,27 @@
+package com.aliouswang.swipeback;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.aliouswang.swipeback.widget.BaseSwipeLayout;
+
+/**
+ * Created by aliouswang on 16/2/18.
+ */
+public class ViewDragerHelperDemoAct extends Activity{
+
+    private BaseSwipeLayout swipe_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_drag_helper_act_layout);
+        swipe_layout = (BaseSwipeLayout) findViewById(R.id.swipe_layout);
+        swipe_layout.setmFinishScroll(new BaseSwipeLayout.OnFinishScroll() {
+            @Override
+            public void finish() {
+                ViewDragerHelperDemoAct.this.finish();
+            }
+        });
+    }
+}
