1929df8a6b3295c614114663098b0066cbcf45b7
aliouswang <wsw622@126.com>
2016-02-21 08:31:49 +0800

    init

--- app/src/main/java/com/aliouswang/swipeback/ViewDragerHelperDemoAct.java
+++ app/src/main/java/com/aliouswang/swipeback/ViewDragerHelperDemoAct.java
@@ -3,6 +3,7 @@ package com.aliouswang.swipeback;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.aliouswang.swipeback.helper.SwipeHelper;
 import com.aliouswang.swipeback.widget.BaseSwipeLayout;
 
 /**
@@ -12,16 +13,19 @@ public class ViewDragerHelperDemoAct extends Activity{
 
     private BaseSwipeLayout swipe_layout;
 
+    private SwipeHelper mSwipeHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_drag_helper_act_layout);
-        swipe_layout = (BaseSwipeLayout) findViewById(R.id.swipe_layout);
-        swipe_layout.setmFinishScroll(new BaseSwipeLayout.OnFinishScroll() {
-            @Override
-            public void finish() {
-                ViewDragerHelperDemoAct.this.finish();
-            }
-        });
+        mSwipeHelper = new SwipeHelper(this);
+        mSwipeHelper.onActivityCreate();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mSwipeHelper.onPostCreate();
     }
 }
--- app/src/main/java/com/aliouswang/swipeback/widget/BaseSwipeLayout.java
+++ app/src/main/java/com/aliouswang/swipeback/widget/BaseSwipeLayout.java
@@ -1,12 +1,15 @@
 package com.aliouswang.swipeback.widget;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Point;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 /**
@@ -126,6 +129,7 @@ public class BaseSwipeLayout extends FrameLayout{
             @Override
             public void onEdgeDragStarted(int edgeFlags, int pointerId) {
                 mCurEdgeFlag = edgeFlags;
+                if (mDragView == null) mDragView = getChildAt(0);
                 mViewDragHelper.captureChildView(mDragView, pointerId);
             }
         });
@@ -154,8 +158,6 @@ public class BaseSwipeLayout extends FrameLayout{
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        mAutoBackOrignalPoint.x = mDragView.getLeft();
-        mAutoBackOrignalPoint.y = mDragView.getTop();
     }
 
     @Override
@@ -174,6 +176,23 @@ public class BaseSwipeLayout extends FrameLayout{
 
     public interface OnFinishScroll {
         void finish();
+    }
+
+    private Activity mActivity;
+    public void attachToActivity(Activity activity) {
+        this.mActivity = activity;
+        TypedArray a = activity.getTheme().obtainStyledAttributes(new int[]{
+                android.R.attr.windowBackground
+        });
+        int background = a.getResourceId(0, 0);
+        a.recycle();
+        ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+        ViewGroup decorChild = (ViewGroup) decorView.getChildAt(0);
+        decorChild.setBackgroundResource(background);
+        decorView.removeView(decorChild);
+        addView(decorChild);
+        decorView.addView(this);
+
     }
 
 }
